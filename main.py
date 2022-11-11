0de5675403965efcc4946e11ed7e7368306af185
kunamax <67630702+kunamax@users.noreply.github.com>
2022-11-11 15:01:42 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,40 +1,31 @@
-from random import randint
-
-
-def Factors(a):
-    b = 2
-    fac1 = 0
-    fac2 = 0
-    while b < a:
-        if a % b == 0 and fac1 == 0:
-            fac1 = b
-            a /= b
-            b -= 1
-        elif a % b == 0 and fac2 == 0:
-            fac2 = b
-            a /= b
-            b -= 1
-        elif a % b == 0:
-
-            return False
-        b += 1
-    return True
-
-
-def Square(l):
+def Sequence(l):
     n = len(l)
-    for i in range(2, n + 1):
-        for x in range(0, n + 1 - i):
-            for y in range(0, n + 1 - i):
-                if Factors(l[x][y] * l[x + i - 1][y] * l[x][y + i - 1] * l[x + i - 1][y + i - 1]):
-                    return i
-    return 0
+    pos1abs = 0
+    pos2abs = 0
+    indicator = True
+    for i in range(n - 1, 2, -1): #chains length
+        for j in range(0, n + 1 - i): #chains first element
+            flag = True
+            pos1 = l[j]
+            pos2 = l[j + i - 1]
+            for k in range(j, j + i - 1): #specific chains elements
+                if l[k] >= l[k + 1]:
+                    flag = False
+            if flag and pos1abs == 0:
+                pos1abs = pos1
+                pos2abs = pos2
+            elif flag and pos1abs != 0:
+                if pos2abs < pos1 or pos1abs > pos2:
+                    return True
+                else:
+                    indicator = False
+        if not indicator:
+            return False
+
+    return False
 
 
 if __name__ == '__main__':
-    size = 4
-    list = [[randint(1, 20) for _ in range(size)] for _ in range(size)]
-    #list = [[17, 1, 13, 16], [7, 3, 20, 2], [5, 10, 1, 13], [10, 7, 3, 16]]
-    for i in list:
-        print(i)
-    print(Square(list))
+    list = [2, 15, 17, 12, 13, 17, 19, 23, 2, 6, 4, 8, 3, 5, 7, 10, 3, 2]
+    n = len(list)
+    print(Sequence(list))
